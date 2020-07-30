a8bacb514f53396814a2a46c05a5488817327cdf
Rafael Jardim <jordao.jardim@hotmail.com.br>
2020-07-30 06:54:23 -0300

    refactor: move validation to its own layer

--- src/infra/db/mongodb/account/account-mongo-repository.ts
+++ src/infra/db/mongodb/account/account-mongo-repository.ts
@@ -1,3 +1,4 @@
+
 import { AddAccountRepository } from '../../../../data/protocols/db/account/add-account-repository'
 import { LoadAccountByEmailRepository } from '../../../../data/protocols/db/account/load-account-by-email-repository'
 import { UpdateAccessTokenRepository } from '../../../../data/protocols/db/account/update-access-token-repository'
