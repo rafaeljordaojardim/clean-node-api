142c689a268454371148257800196be5a08262fa
Rafael Jardim <jordao.jardim@hotmail.com.br>
2020-08-05 05:58:40 -0300

    feat: ensure DbLoadAccountByToken calls decrypter with correct value

--- src/presentation/middlewares/auth-middleware.ts
+++ src/presentation/middlewares/auth-middleware.ts
@@ -1,6 +1,6 @@
+import { HttpRequest, HttpResponse, Middleware, LoadAccountByToken } from './auth-middleware-protocols'
 import { AccessDeniedError } from '../errors'
 import { forbidden, ok, serverError } from '../helpers/http/http-helper'
-import { HttpRequest, HttpResponse, Middleware, LoadAccountByToken } from './auth-middleware-protocols'
 
 export class AuthMiddleware implements Middleware {
   constructor (
