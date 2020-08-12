3e218d4b83df8e9793dff5e305b90f83ce390407
Rafael Jardim <jordao.jardim@hotmail.com.br>
2020-08-12 06:56:34 -0300

    feat: ensure DbLoadSurveyById calls LoadSurveyByIdRepository with correct id

---
+++ src/data/usecases/load-surveys-by-id/db-load-survety-by-id.ts
@@ -0,0 +1,11 @@
+import { LoadSurveyByIdRepository } from '@/data/protocols/db/survey/load-survey-by-id-repository'
+import { SurveyModel } from '@/domain/models/survey'
+import { LoadSurveyById } from '@/domain/usecases/load-survey-by-id'
+
+export class DbLoadSurveyById implements LoadSurveyById {
+  constructor (private readonly loadSurveyById: LoadSurveyByIdRepository) {}
+  async loadById (id: string): Promise<SurveyModel> {
+    await this.loadSurveyById.loadById(id)
+    return null
+  }
+}
