0a2a7dd743c49f61e84fbb1c22c93019602812ad
Rafael Jardim <jordao.jardim@hotmail.com.br>
2020-08-12 22:15:36 -0300

    test: ensure SUrveyResultMOngoRepository insert updates surveyResult if its not new

--- src/infra/db/mongodb/survey-result/survey-result-mongo-repository.spec.ts
+++ src/infra/db/mongodb/survey-result/survey-result-mongo-repository.spec.ts
@@ -15,6 +15,9 @@ const makeSurvey = async (): Promise<SurveyModel> => {
     answers: [{
       image: 'any_image',
       answer: 'any_answer'
+    }, {
+      image: 'othery_image',
+      answer: 'other_answer'
     }],
     date: new Date()
   })
@@ -60,6 +63,27 @@ describe('Survey Mongo Repository', () => {
       expect(surveyResult).toBeTruthy()
       expect(surveyResult.id).toBeTruthy()
       expect(surveyResult.answer).toBe(survey.answers[0].answer)
+    })
+
+    test('Should update a survey result if its not new', async () => {
+      const survey = await makeSurvey()
+      const account = await makeAccount()
+      const res = await surveyResultCollection.insertOne({
+        surveyId: survey.id,
+        accountId: account.id,
+        answer: survey.answers[0].answer,
+        date: new Date()
+      })
+      const sut = makeSut()
+      const surveyResult = await sut.save({
+        surveyId: survey.id,
+        accountId: account.id,
+        answer: survey.answers[1].answer,
+        date: new Date()
+      })
+      expect(surveyResult).toBeTruthy()
+      expect(surveyResult.id).toEqual(res.ops[0]._id)
+      expect(surveyResult.answer).toBe(survey.answers[1].answer)
     })
   })
 })
