cef9df4e07769f40ac176f0ff0ccacc38c758ef2
Rafael Jardim <jordao.jardim@hotmail.com.br>
2020-08-13 20:21:25 -0300

    feat: ensure save survey result route returns 403 if no token is provided

---
+++ src/main/factories/controllers/survey-result/save-survey-result/save-survey-result-controller-factory.ts
@@ -0,0 +1,10 @@
+import { Controller } from '@/presentation/protocols'
+import { makeLogControllerDecorator } from '../../../decorators/log-controller-decorator-factory'
+import { SaveSurveyResultController } from '@/presentation/controllers/survey-result/save-survey-result/save-survey-result-controller'
+import { makeDbLoadSurveyById } from '@/main/factories/usecases/survey/load-survey-by-id/db-load-surveys-factory'
+import { makeDbSaveSurveyResult } from '@/main/factories/usecases/survey-result/save-survey-result/db-save-survey-result-factory'
+
+export const makeSurveyResultController = (): Controller => {
+  const controller = new SaveSurveyResultController(makeDbLoadSurveyById(), makeDbSaveSurveyResult())
+  return makeLogControllerDecorator(controller)
+}
---
+++ src/main/routes/survey-result-routes.ts
@@ -0,0 +1,8 @@
+import { makeSurveyResultController } from '@/main/factories/controllers/survey-result/save-survey-result/save-survey-result-controller-factory'
+import { adaptRoute } from '../adapters/express-route-adapter'
+import { auth } from '../middlewares/auth'
+import { Router } from 'express'
+
+export default (router: Router): void => {
+  router.put('/surveys/:surveyId/results', auth, adaptRoute(makeSurveyResultController()))
+}
