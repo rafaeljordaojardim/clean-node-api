f51b94602652a11c4459da0805c91d83c7590336
Rafael Jardim <jordao.jardim@hotmail.com.br>
2020-08-13 21:57:49 -0300

    feat: ensure save survey result route returns 200 on success

--- src/presentation/controllers/survey-result/save-survey-result/save-survey-result-controller.ts
+++ src/presentation/controllers/survey-result/save-survey-result/save-survey-result-controller.ts
@@ -14,6 +14,7 @@ export class SaveSurveyResultController implements Controller {
       const { answer } = httpRequest.body
       const { accountId } = httpRequest
       const survey = await this.loadSurveyById.loadById(surveyId)
+
       if (survey) {
         const answers = survey.answers.map(a => a.answer)
         if (!answers.includes(answer)) {
