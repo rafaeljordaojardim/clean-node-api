01d1b76bf151ef2ed4f4fbd990a3009c2556aed1
Rafael Jardim <jordao.jardim@hotmail.com.br>
2020-08-18 16:52:57 -0300

    docs: add signup api docs

--- src/main/docs/index.ts
+++ src/main/docs/index.ts
@@ -1,6 +1,6 @@
-import { loginPath, surveyPath } from './paths/'
+import { loginPath, surveyPath, signUpPath } from './paths/'
 import { badRequest, serverError, unauthorized, notFound, forbidden } from './components/'
-import { accountSchema, errorSchema, loginParamsSchema, surveyAnswerSchema, surveySchema, surveysSchema, apiKeyAuthSchema } from './schemas/'
+import { accountSchema, errorSchema, loginParamsSchema, surveyAnswerSchema, surveySchema, surveysSchema, apiKeyAuthSchema, signupParamsSchema } from './schemas/'
 
 export default {
   openapi: '3.0.0',
@@ -23,11 +23,13 @@ export default {
   }],
   paths: {
     '/login': loginPath,
+    '/signup': signUpPath,
     '/surveys': surveyPath
   },
   schemas: {
     account: accountSchema,
     loginParams: loginParamsSchema,
+    signUpParams: signupParamsSchema,
     error: errorSchema,
     surveys: surveysSchema,
     survey: surveySchema,
