4246543ab80d32ae3a1f32a13941f8eebba4362a
Rafael Jardim <jordao.jardim@hotmail.com.br>
2020-08-18 06:40:37 -0300

    docs: update login API

--- src/main/docs/index.ts
+++ src/main/docs/index.ts
@@ -1,6 +1,6 @@
-import { loginPath } from './paths/login-path'
-import { accountSchema } from './schemas/account-schema'
-import { loginParamsSchema } from './schemas/login-params-schema'
+import { loginPath } from './paths/'
+import { badRequest, serverError, unauthorized, notFound } from './components/'
+import { accountSchema, errorSchema, loginParamsSchema } from './schemas/'
 
 export default {
   openapi: '3.0.0',
@@ -8,6 +8,10 @@ export default {
     title: 'Clean API',
     description: 'Clean nodejs',
     version: '1.0.0'
+  },
+  license: {
+    name: 'ISC',
+    url: ''
   },
   servers: [{
     url: '/api'
@@ -20,6 +24,13 @@ export default {
   },
   schemas: {
     account: accountSchema,
-    loginParams: loginParamsSchema
+    loginParams: loginParamsSchema,
+    error: errorSchema
+  },
+  components: {
+    badRequest,
+    serverError,
+    unauthorized,
+    notFound
   }
 }
